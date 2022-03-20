5e00ae9451ceb2458e3d64c6356740883479d051
mjb34b <mjb34b@umsl.edu>
2022-03-19 22:54:23 -0500

    Get URL from Tweet ID

--- main.py
+++ main.py
@@ -6,4 +6,23 @@ auth.set_access_token(config.access_token, config.access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
+vidToGet = input("Enter tweet ID:")
+tweet = api.get_status(vidToGet, tweet_mode="extended")
+vidCheck = ''
+bitrate = 0
+url = ''
+for tweetType in tweet.extended_entities['media']:
+    vidCheck = tweetType['type']
+if vidCheck == "video":
+    for vid in tweet.extended_entities['media']:
+        vidin = vid.get('video_info')
+        for i in vidin.get('variants'):
+            if i.get('content_type') == 'video/mp4':
+                if i.get('bitrate') > bitrate:
+                    bitrate = i.get('bitrate')
+                    url = i.get('url')
+
+else:
+    print("Tweet does not contain a video.")
+
+print(url)
