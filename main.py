0b97b2b09c746b9eda8b8fa388178a35a36c2d7d
mjb34b <mjb34b@umsl.edu>
2022-03-19 19:13:55 -0500

    Added security

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import tweepy
+import config
 
-consumer_key = input("Consumer Key")
-consumer_secret = input("Secret Key")
-access_token = input("Access Token")
-access_token_secret = input("Access Secret")
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
 
 api = tweepy.API(auth)
 
 public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
